22098caf04dd068ea1f23acb522516e7b33fe1a8
Fumiya Funatsu <funatsu.fumiya@gmail.com>
2026-05-15 04:04:15 +0900

    update sh

--- create_releases.sh
+++ create_releases.sh
@@ -7,17 +7,20 @@ function create_tar_gz() {
     ext=$2
     name_arg=$3
     name=${name_arg:="$LIB"}
+    echo "creating $target.tar.gz from $name.$ext"
     rm -rf $target
     mkdir $target
     mkdir $target/lib
     mkdir $target/include
     cp libs/$name.$ext $target/lib
     cd $target
-    tar czf $target.tar.gz *
+    tar czf ../$target.tar.gz *
     cd ..
     rm -rf $target
 }
 
+cd `dirname $0`
+echo "working dir: $(pwd)"
 rm -rf *.tar.gz
 create_tar_gz "mac_arm64" "dylib"
 create_tar_gz "linux_arm64" "so"
