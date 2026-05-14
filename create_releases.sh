805807f777cd466235db9a68634ada604d25d471
Fumiya Funatsu <funatsu.fumiya@gmail.com>
2026-05-15 03:52:18 +0900

    add sh

---
+++ create_releases.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+LIB=libJlTrussC
+
+function create_tar_gz() {
+    target=$1
+    ext=$2
+    name=${3:="$LIB"}
+    rm -rf $target
+    mkdir $target
+    mkdir $target/lib
+    mkdir $target/include
+    cp libs/$name.$ext $target/lib
+    tar czf $target.tar.gz $target
+    rm -rf $target
+}
+
+create_tar_gz "mac_arm64" "dylib"
+create_tar_gz "linux_arm64" "so"
+create_tar_gz "windows_arm64" "dll"
